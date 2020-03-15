5648b94557745586c169cc47eac443b62471f43d
Gneffi <37945505+AkiiWakii@users.noreply.github.com>
2020-03-15 18:49:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//Question 1
+var petArray = ["cat","cow","dog"];
+//Question 2
+console.log(petArray);
+//Question 3
+petArray.push("Sheep");
+//Question 4
+console.log(petArray.length);
+//Question 5
+var catObject = {
+    name:"Garfield",
+    colour: "Orange",
+    age: 2
+};
+//Question 6
+console.log(catObject.age)
+//Question 7
+var catArray = [
+    {
+        name:"Garfield",
+        colour: "Orange",
+        age: 2
+    }
+];
+//Question 8
+var firstItem = catArray[0]
+console.log(firstItem);
+
+//Question 9
+function logToConsole(){
+   console.log(catArray[0]) 
+};
+//Question 10
+logToConsole();
